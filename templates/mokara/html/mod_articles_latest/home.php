64e2ab756c334d27d2f6c335043840119d38e057
eddynguyen <email@giahuy10.com>
2017-06-01 11:11:17 +0700

    update

--- templates/mokara/html/mod_articles_latest/home.php
+++ templates/mokara/html/mod_articles_latest/home.php
@@ -18,7 +18,7 @@ $model = JModelLegacy::getInstance('Product', 'MokaraModel', array('ignore_reque
 	$clear=0;
 foreach ($list as $item) : ?>
 	
-	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 items-on-row" itemscope itemtype="https://schema.org/Article">
+	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 items-on-row">
 	
 		<?php $model->show_product_item($item)?>
 		
