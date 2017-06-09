f2df3591817c21b06bd5f54283fe711bda2ac47c
eddynguyen <email@giahuy10.com>
2017-06-09 22:49:23 +0700

    sh404 update

--- administrator/components/com_sh404sef/plugins/sh404sefcore/sh404sefsimilarurls/sh404sefsimilarurls.php
+++ administrator/components/com_sh404sef/plugins/sh404sefcore/sh404sefsimilarurls/sh404sefsimilarurls.php
@@ -303,7 +303,24 @@ function shEscapeLike($string, $db)
 
 	return $escaped;
 }
-
+function pageTitle($page_url)
+{
+     $read_page=file_get_contents($page_url);
+     preg_match("/<title.*?>[\n\r\s]*(.*)[\n\r\s]*<\/title>/", $read_page, $page_title);
+      if (isset($page_title[1]))
+      {
+            if ($page_title[1] == '')
+            {
+                  return $page_url;
+            }
+            $page_title = $page_title[1];
+            return trim($page_title);
+      }
+      else
+      {
+            return $page_url;
+      }
+}
 function shFormatSimilarUrls($urlList)
 {
 	$urls = '';
@@ -311,7 +328,7 @@ function shFormatSimilarUrls($urlList)
 	{
 		foreach ($urlList as $url)
 		{
-			$urls .= '<li><a href="' . JRoute::_($url->newurl) . '">' . $url->oldurl . '</a></li>';
+			$urls .= '<li><a href="' . JRoute::_($url->newurl) . '">' . pageTitle(JURI::root().$url->oldurl) . '</a></li>';
 		}
 		$urls = '<ul>' . $urls . '</ul>';
 	}
